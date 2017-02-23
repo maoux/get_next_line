c0829a9cfb09bb364d0e12ee38e192667182afdf
Hugo EYNARD <heynard@e1r1p3.42.fr>
2017-02-23 17:15:47 +0100

    final push

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: heynard <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 13:32:44 by heynard           #+#    #+#             */
-/*   Updated: 2017/02/23 17:05:37 by heynard          ###   ########.fr       */
+/*   Updated: 2017/02/23 17:15:31 by heynard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 2
+# define BUFF_SIZE 8
 
 # include "libft/includes/libft.h"
 # include <sys/uio.h>
